363b6eadda6f84e50d439aa5b95d5b670ab45085
JnyJny <erik.oshaughnessy@gmail.com>
2025-06-01 20:12:08 -0500

    Bugfix in template __main__.py
    The ctx.obj instance is None and has no settings attribute. Just assign the settings class instance to ctx.obj.

--- {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/__main__.py
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/__main__.py
@@ -23,8 +23,8 @@ def global_callback(
 ) -> None:
     """{{ cookiecutter.project_short_description }}
     """
-{% if cookiecutter.use_pydantic_settings == "yes" %}    ctx.obj.settings = {{ cookiecutter.package_name.title() }}Settings()
-    debug = debug or ctx.obj.settings.debug{% endif %}
+{% if cookiecutter.use_pydantic_settings == "yes" %}    ctx.obj = {{ cookiecutter.package_name.title() }}Settings()
+    debug = debug or ctx.obj.debug{% endif %}
     (logger.enable if debug else logger.disable)("{{ cookiecutter.package_name }}")
 {% if cookiecutter.log_to_file == "yes" %}    logger.add("{{ cookiecutter.package_name }}.log"){% endif %}    
     logger.info(f"{debug=}")
