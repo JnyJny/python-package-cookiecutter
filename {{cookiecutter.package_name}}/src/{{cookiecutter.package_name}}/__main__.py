3356b67efeebd1cc2e0dd3658e7037cf49dbce36
JnyJny <erik.oshaughnessy@gmail.com>
2025-06-01 20:08:07 -0500

    Update template __main__.py
    Added support for optional logging to file and using pydantic settings.

--- {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/__main__.py
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/__main__.py
@@ -8,6 +8,8 @@ import sys
 import typer
 from loguru import logger
 
+{% if cookiecutter.use_pydantic_settings == "yes" %}from .settings import {{ cookiecutter.package_name.title() }}Settings{% endif %}
+
 cli = typer.Typer()
 
 @cli.callback(invoke_without_command=True, no_args_is_help=True)
@@ -21,9 +23,11 @@ def global_callback(
 ) -> None:
     """{{ cookiecutter.project_short_description }}
     """
+{% if cookiecutter.use_pydantic_settings == "yes" %}    ctx.obj.settings = {{ cookiecutter.package_name.title() }}Settings()
+    debug = debug or ctx.obj.settings.debug{% endif %}
     (logger.enable if debug else logger.disable)("{{ cookiecutter.package_name }}")
+{% if cookiecutter.log_to_file == "yes" %}    logger.add("{{ cookiecutter.package_name }}.log"){% endif %}    
     logger.info(f"{debug=}")
-
 
 
 if __name__ == "__main__":
