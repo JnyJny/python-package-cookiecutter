5242c47003648c6517233598902d7b7e0da56f18
JnyJny <erik.oshaughnessy@gmail.com>
2025-06-06 14:27:05 -0500

    Updated template __main__.py
    Used my Jinja whitespace magic to cleanup the __main__ module.

--- {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/__main__.py
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/__main__.py
@@ -8,25 +8,31 @@ import sys
 import typer
 from loguru import logger
 
-{% if cookiecutter.use_pydantic_settings == "yes" %}from .settings import {{ cookiecutter.package_name.title() }}Settings{% endif %}
+{%- if cookiecutter.use_pydantic_settings == "yes" %}
+from .settings import {{ cookiecutter.package_name.title() }}Settings
+{%- endif %}
 
 cli = typer.Typer()
 
 @cli.callback(invoke_without_command=True, no_args_is_help=True)
 def global_callback(
-        ctx: typer.Context,
-        debug: bool = typer.Option(
-            False,
-            "--debug",
-            '-D',
-            help="Enable debugging output"),
+    ctx: typer.Context,
+    debug: bool = typer.Option(
+        False,
+        "--debug",
+        '-D',
+        help="Enable debugging output"),
 ) -> None:
     """{{ cookiecutter.project_short_description }}
     """
-{% if cookiecutter.use_pydantic_settings == "yes" %}    ctx.obj = {{ cookiecutter.package_name.title() }}Settings()
-    debug = debug or ctx.obj.debug{% endif %}
+{%- if cookiecutter.use_pydantic_settings == "yes" %}
+     ctx.obj = {{ cookiecutter.package_name.title() }}Settings()
+    debug = debug or ctx.obj.debug
+{%- endif %}
     (logger.enable if debug else logger.disable)("{{ cookiecutter.package_name }}")
-{% if cookiecutter.log_to_file == "yes" %}    logger.add("{{ cookiecutter.package_name }}.log"){% endif %}    
+{%- if cookiecutter.log_to_file == "yes" %}
+    logger.add("{{ cookiecutter.package_name }}.log")
+{% endif -%}    
     logger.info(f"{debug=}")
 
 
